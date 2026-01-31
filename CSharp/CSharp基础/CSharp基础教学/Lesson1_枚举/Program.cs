b37789ca392d92daac2e741dac277a2b0131080b
SloaAya <1587198980@qq.com>
2026-01-31 20:50:01 +0800

    Code

---
+++ CSharp/CSharp基础/CSharp基础教学/Lesson1_枚举/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson1_枚举
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("枚举");
+            #region 知识点一 基本概念
+            #region 枚举是什么
+            //枚举是一个比较特别的存在
+            //它是一个被命名的整形常量的集合
+            //一般用它来表示 状态 类型 等等
+            #endregion
+            #endregion
+
+            #region 知识点二 在哪申明枚举
+
+            #endregion
+
+            #region 知识点三 枚举的使用
+
+            #endregion
+
+            #region 知识点四 枚举的类型
+
+            #endregion
+
+            #region 知识点五 枚举的作用
+
+            #endregion
+        }
+    }
+}
